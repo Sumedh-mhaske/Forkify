846767fe4369bc76682385bd4a92d652b1b5aa89
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-09-04 16:19:31 +0530

    Rendering loading spinner & importing some modules

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,8 @@
+// import icons from '../img/icons.svg'; // Parcel 1
+import icons from 'url:../img/icons.svg'; // Parcel 2
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,11 +17,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-// https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
 
 const showRecipe = async function () {
   try {
     // 1) Loading recipe
+    renderSpinner(recipeContainer);
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8a12'
@@ -52,7 +68,7 @@ const showRecipe = async function () {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
+              <use href="${recipe.icons}#icon-clock"></use>
             </svg>
           <span class="recipe__info-data recipe__info-data--minutes">${
             recipe.cookingTime
@@ -61,7 +77,7 @@ const showRecipe = async function () {
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
+              <use href="${recipe.icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
               recipe.servings
@@ -71,12 +87,12 @@ const showRecipe = async function () {
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                  <use href="${recipe.icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href="${recipe.icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -84,12 +100,12 @@ const showRecipe = async function () {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
+              <use href="${recipe.icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${recipe.icons}#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
@@ -102,7 +118,7 @@ const showRecipe = async function () {
                 return `
                   <li class="recipe__ingredient">
                     <svg class="recipe__icon">
-                      <use href="src/img/icons.svg#icon-check"></use>
+                      <use href="${recipe.icons}#icon-check"></use>
                     </svg>
                     <div class="recipe__quantity">${ing.quantity}</div>
                     <div class="recipe__description">
@@ -133,7 +149,7 @@ const showRecipe = async function () {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
+              <use href="${recipe.icons}#icon-arrow-right"></use>
             </svg>
           </a>
         </div>
