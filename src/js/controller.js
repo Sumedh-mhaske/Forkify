8566202f9f1fa6b198d37ed84798e169b4bd21f1
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-09-17 20:52:31 +0530

    Rendering search result in console

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-import SearchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -25,18 +24,23 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    // 2) Load serach results
     await model.loadSearchResults(query);
+
+    // 3) Render results
     console.log(model.state.search.result);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,7 +2,20 @@ class Searchview {
   #parentEl = document.querySelector('.search');
 
   getQuery() {
-    return this.#parentEl.querySelector('.search_field').value;
+    const query = this.#parentEl.querySelector('.search__field').value;
+    this.#clearInput();
+    return query;
+  }
+
+  #clearInput() {
+    this.#parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this.#parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
   }
 }
 
